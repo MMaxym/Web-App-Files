cc7d2b19152b225798ed0a3ff4e4abd9fc05d859
Maxym <maxym@Admins-MacBook-Pro.local>
2025-01-29 16:47:08 +0200

    WAF-10 Create table models

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class File extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'file_name',
+        'file_path',
+        'comment',
+        'expiration_date',
+        'views_count',
+    ];
+
+    protected $casts = [
+        'expiration_date' => 'date',
+        'user_id' => 'integer',
+        'views_count' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function fileLinks(): HasMany
+    {
+        return $this->hasMany(FileLink::class);
+    }
+
 }
--- app/Models/FileLink.php
+++ app/Models/FileLink.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FileLink extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'file_id',
+        'type',
+        'token',
+        'is_active',
+        'views_count',
+    ];
+
+    protected $casts = [
+        'file_id' => 'integer',
+        'is_active' => 'boolean',
+        'views_count' => 'integer',
+        'type' => 'enum',
+    ];
+
+    public function file(): BelongsTo
+    {
+        return $this->belongsTo(File::class);
+    }
 }
