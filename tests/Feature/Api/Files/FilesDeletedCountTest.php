e3258929826a3a7db1c822db854a053e94586353
Maxym <maxym@Admins-MacBook-Pro.local>
2025-04-01 13:18:02 +0300

    WAF-53 Add test api endpoint filesDeletedCount

--- tests/Feature/Api/Files/FilesDeletedCountTest.php
+++ tests/Feature/Api/Files/FilesDeletedCountTest.php
@@ -8,30 +8,31 @@ use App\Models\File;
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase;
 
-class FilesExistingCountTest extends TestCase
+class FilesDeletedCountTest extends TestCase
 {
-    public function test_user_can_get_existing_count_files(): void
+    public function test_user_can_get_deleted_count_files(): void
     {
         $user = User::factory()->create();
 
         File::factory(5)->create([
             'user_id' => $user->id,
+            'deleted_at' => now(),
         ]);
 
-        $response = $this->get('api/users/'.$user->id.'/files/existing-count');
+        $response = $this->get('api/users/'.$user->id.'/files/deleted-count');
 
         $response->assertStatus(200)
             ->assertJson([
                 'success' => true,
             ])
             ->assertJsonFragment([
-                'existing_files_count' => intval($response->json('existing_files_count')),
+                'deleted_files_count' => intval($response->json('deleted_files_count')),
             ]);
     }
 
-    public function test_user_can_get_existing_count_files_fails_invalid_id(): void
+    public function test_user_can_get_deleted_count_files_fails_invalid_id(): void
     {
-        $response = $this->get('api/users/00/files/existing-count');
+        $response = $this->get('api/users/00/files/deleted-count');
 
         $response->assertStatus(404)
             ->assertJson([
